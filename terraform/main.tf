34527b9458b13e10f4b53eb112b21f3f2283dab1
Mostafa Elshwaihi <128544423+elshwaihy@users.noreply.github.com>
2024-10-04 01:10:31 +0300

    Update main.tf
    add script to install docker

--- terraform/main.tf
+++ terraform/main.tf
@@ -4,6 +4,22 @@ resource "aws_instance" "ubuntu-instance" {
   key_name      = "mykey"
   security_groups = ["${aws_security_group.UbuntuSG.name}"]
 
+  # Add shell script to install Docker
+  user_data = <<-EOF
+    #!/bin/bash
+    sudo apt-get update
+    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+    sudo apt-get update
+    sudo apt-get install -y docker-ce
+    sudo systemctl start docker
+    sudo systemctl enable docker
+    sudo groupadd docker
+    sudo usermod -aG docker $USER && newgrp docker
+    sudo chmod 777 /var/run/docker.sock
+  EOF
+
   tags  = {
     Name  = "Ubuntu-EC2"
   }
