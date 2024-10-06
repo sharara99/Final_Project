c695fa32fa606f243b3f79934c83d98f2051b514
Muhammad Elmansi <muhammad.elmansi7@gmail.com>
2024-10-06 16:26:41 +0300

    update main file

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,26 +6,28 @@ resource "aws_instance" "ubuntu-instance" {
 
   # Add shell script to install Docker
   user_data = <<-EOF
-    #!/bin/bash
-    sudo apt-get update
-    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-    sudo apt-get update
-    sudo apt-get install -y docker-ce
-    sudo systemctl start docker
-    sudo systemctl enable docker
-    sudo groupadd docker
-    sudo usermod -aG docker $USER && newgrp docker
-    sudo chmod 777 /var/run/docker.sock
-  EOF
+                    #!/bin/bash
+                    sudo apt-get update
+                    sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+                    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+                    sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+                    sudo apt-get update
+                    sudo apt-get install -y docker-ce
+                    sudo systemctl start docker
+                    sudo systemctl enable docker
+                    sudo groupadd docker
+                    sudo usermod -aG docker $USER && newgrp docker
+                    sudo chmod 777 /var/run/docker.sock
+                  EOF
 
   tags  = {
     Name  = "Ubuntu-EC2"
   }
 
   # Create inventory file for ansible
-  provisioner "local-exec" {
-    command = "echo '[my_ec2]' > ../inventory && echo ${self.public_ip} >> ../inventory"
+    provisioner "local-exec" {
+    command = "echo '[my_ec2]' > ../inventory.txt && echo '${self.public_ip}' >> ../inventory.txt"
   }
+
+
 }
